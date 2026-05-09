80be402160bd5db3ff288248bc614fa95f317a0e
JLChambi <lunaxsbs@gmail.com>
2026-05-09 17:21:41 -0400

    config: actualizar dependencias y configuraciones de build

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'role' => \App\Http\Middleware\CheckRole::class,
+        ]);
+        $middleware->web(append: [
+            \App\Http\Middleware\HandleInertiaRequests::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
